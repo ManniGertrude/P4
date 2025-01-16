a4a8a7b507b8d85b75fa58665648a8cd57204e1f
ManniGertrude <mannigertrude@gmail.com>
2025-01-16 10:10:23 +0100

    modified:   428/Gitter.py 	modified:   428/NaCl281.97.png

--- 428/Gitter.py
+++ 428/Gitter.py
@@ -16,19 +16,28 @@ def gausfit(func, x, y, farbe, beta):
     myodr = odr.ODR(mydata, model, beta0=beta, maxit=1000)
     out = myodr.run()
     
+    print(nm2Ev(out.beta[1]))
+    
     fy = func(out.beta, x)
     # print('$Parameter:', out.beta, out.sd_beta,  '$')
     chi2 = np.sum((y - fy)**2/fy)
-    print(sum((y-fy)**2/fy))
+    # print(sum((y-fy)**2/fy))
     ndf = len(x) - len(out.beta)
     plt.plot(x, fy, c=farbe,label = f'Gauß-Anpassung mit $\chi^2/ndf = {chi2/ndf:.3g} $', alpha=0.7)
     return out
 
 def Ang2nm(x):
-    return 2*281.97*np.sin(x/2)
+    return 2*562*np.sin(np.deg2rad(x/2))
 
 def nm2Ang(x):
-    return 2*np.arcsin(x/(2*281.97))
+    return 2*np.arcsin(np.deg2rad(x/(2*562)))
+
+def nm2Ev(x):
+    return 1239.8/Ang2nm(x)
+
+def Ev2nm(x):
+    return nm2Ang(x)*1239.8
+
 
 path = os.path.dirname(os.path.abspath(__file__))
 data = pd.read_csv(f'{path}\\DatenD1\\NaCl281.97.txt', sep="\t", header=0, names=['b', 'R'], dtype=str)
@@ -52,7 +61,10 @@ gausfit(gauss, xP1, yP1, 'blue', [200, 18.25, 0.3, 0, 36.5])
 plt.ylabel('Mittlere Zählrate [1/s]', fontsize=12)
 plt.xlabel('Winkel [°]', fontsize=12)
 secax = ax.secondary_xaxis('top', functions=(Ang2nm, nm2Ang))
-secax.set_xlabel('Wellenlänge [nm]', fontsize=12)
+thiax = ax.secondary_xaxis( 0.5, functions=(nm2Ev, Ev2nm))
+secax.set_xlabel('Wellenlänge [pm]', fontsize=12)
+thiax.set_xlabel('Energie [keV]', fontsize=12)
+# ax.axvline(18.25, color='blue', linestyle='--', label='Position 1')
 plt.legend()
 plt.grid()
 plt.savefig(f'{path}\\NaCl281.97.png')
