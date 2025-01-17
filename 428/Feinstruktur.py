8f00c57ae467ba4cd9e773c6ca607db5ba5b1f4d
ManniGertrude <mannigertrude@gmail.com>
2025-01-18 00:33:07 +0100

    modified:   428/Feinstruktur.pdf 	modified:   428/Feinstruktur.py 	new file:   428/FeinstrukturZoom.pdf 	new file:   428/Peaks/1unbek.peaks 	new file:   428/Peaks/2unbek.peaks 	new file:   428/Peaks/3unbek.peaks 	new file:   428/Peaks/4unbek.peaks 	new file:   428/Peaks/AgK.peaks 	new file:   428/Peaks/AgL.peaks 	new file:   428/Peaks/Au.peaks 	new file:   428/Peaks/Cu.peaks 	new file:   428/Peaks/Fe.peaks 	new file:   428/Peaks/FeZn.peaks 	new file:   428/Peaks/In.peaks 	new file:   428/Peaks/Mo.peaks 	new file:   428/Peaks/Ni.peaks 	new file:   428/Peaks/Pb.peaks 	new file:   428/Peaks/Sn.peaks 	new file:   428/Peaks/Ti.peaks 	new file:   428/Peaks/W.peaks 	new file:   428/Peaks/Zn.peaks 	new file:   428/Peaks/Zr.peaks 	modified:   428/Plots/1unbek.pdf 	modified:   428/Plots/2unbek.pdf 	modified:   428/Plots/3unbek.pdf 	modified:   428/Plots/4unbek.pdf 	modified:   428/Plots/AgK.pdf 	modified:   428/Plots/AgL.pdf 	modified:   428/Plots/Au.pdf 	modified:   428/Plots/Cu.pdf 	modified:   428/Plots/Fe.pdf 	modified:   428/Plots/FeZn.pdf 	deleted:    428/Plots/Feinstruktur.pdf 	modified:   428/Plots/In.pdf 	modified:   428/Plots/Mo.pdf 	deleted:    428/Plots/NaCl281.97.pdf 	modified:   428/Plots/Ni.pdf 	modified:   428/Plots/PB.pdf 	modified:   428/Plots/Sn.pdf 	modified:   428/Plots/Ti.pdf 	modified:   428/Plots/W.pdf 	modified:   428/Plots/Zn.pdf 	modified:   428/Plots/Zr.pdf 	modified:   428/Spektren.py

--- 428/Feinstruktur.py
+++ 428/Feinstruktur.py
@@ -16,23 +16,23 @@ def gauss(Para, x):
 
 def gausfit(func, x, y, farbe, beta):
     model = odr.Model(func)
-    mydata = odr.RealData(x, y, sx=0.005, sy=np.sqrt(y)/120)
+    mydata = odr.RealData(x, y, sx=0.005, sy=np.sqrt(120*y)/120)
     myodr = odr.ODR(mydata, model, beta0=beta, maxit=10000)
     out = myodr.run()
-    print(f'E in keV: {Deg2Ev(out.beta[1]):.8g}+-{(np.deg2rad(out.sd_beta[1])*0.55151245516*np.cos(np.deg2rad(out.beta[1]/2))/np.sin(np.deg2rad(out.beta[1]/2))**2):.2g}')
-    print(f'lambda in pm: {Deg2pm(out.beta[1]):.8g}+-{(np.deg2rad(out.sd_beta[1])*0.55151245516/np.sin(np.deg2rad(out.beta[1]/2))**2):.2g}')
+    # print(f'Winkel {out.beta[1]} $\pm$ {out.sd_beta[1]}')
+    # print(f'E in keV: {Deg2Ev(out.beta[1]):.8g}$\pm$ {(np.deg2rad(out.sd_beta[1])*0.55151245516*np.cos(np.deg2rad(out.beta[1]/2))/np.sin(np.deg2rad(out.beta[1]/2))**2):.2g}')
+    # print(f'lambda in pm: {Deg2pm(out.beta[1]):.8g}$\pm$ {(np.deg2rad(out.sd_beta[1])*0.55151245516/np.sin(np.deg2rad(out.beta[1]/2))**2):.2g}')
     fy = func(out.beta, x)
-    # Printer(f'{out.beta[0]:.5g} $\pm$ {out.sd_beta[0]:.3g} & {out.beta[1]:.5g} $\pm$ {out.sd_beta[1]:.3g} & {out.beta[2]:.5g} $\pm$ {out.sd_beta[2]:.3g} & {out.beta[3]:.5g} $\pm$ {out.sd_beta[3]:.3g} & {out.beta[4]:.5g} $\pm$ {out.sd_beta[4]:.3g} & {np.sum((y - fy)**2/fy)/(len(x) - len(out.beta)):.3g}')
-    chi2 = np.sum((y - fy)**2/fy)
-    ndf = len(x) - len(out.beta)
-    plt.plot(x, fy, c=farbe,label = f'Gauß-Anpassung mit $\chi^2/ndf = {chi2/ndf:.3g} $', alpha=0.7)
+    chi2 = np.sum((y - fy)**2 *120/np.sqrt(y))/(len(x) - len(out.beta))
+    # Printer(f'{out.beta[0]:.5g} $\pm$ {out.sd_beta[0]:.3g} & {out.beta[1]:.5g} $\pm$ {out.sd_beta[1]:.3g} & {out.beta[2]:.5g} $\pm$ {out.sd_beta[2]:.3g} & {out.beta[3]:.5g} $\pm$ {out.sd_beta[3]:.3g} & {out.beta[4]:.5g} $\pm$ {out.sd_beta[4]:.3g} & {chi2:.3g}')
+    plt.plot(x, fy, c=farbe,label = f'Gauß-Anpassung mit $\chi^2/ndf = {chi2:.3g} $', alpha = 0.7)
     return out
 
 def Deg2pm(x):
-    return 2*562.73*np.sin(np.deg2rad(x/2))/1.467
+    return 2*562.73*np.sin(np.deg2rad(x/2))/4
 
 def pm2Deg(x):
-    return np.rad2deg(2*np.arcsin(1.467*x/(2*562.73)))
+    return np.rad2deg(2*np.arcsin(4*x/(2*562.73)))
 
 def Deg2Ev(x):
     return 1239.8/Deg2pm(x)
@@ -50,32 +50,51 @@ data['R'] = data['R'].str.replace(',', '.').astype(float)
 xData = data['b'].values
 yData = data['R'].values
 
-xP1 = xData[150:172]
-yP1 = yData[150:172]
+xP1 = xData[75:150]
+yP1 = yData[75:150]
 
-xP2 = xData[170:215]
-yP2 = yData[170:215]
+xP2 = xData[280:340]
+yP2 = yData[280:340]
 
 
-plt.errorbar(xData, yData, xerr=0.005, yerr=np.sqrt(yData)/120, color='darkolivegreen', label='Messwerte', marker='.', zorder=1, linestyle='None')
-# gausfit(gauss, xP2, yP2, 'purple', [426, 20, 0.18, 0, 36.5])
-# gausfit(gauss, xP1, yP1, 'blue', [200, 18.25, 0.3, 0, 36.5])
-
+plt.errorbar(xData, yData, xerr=0.005, yerr=np.sqrt(120*yData)/120, color='darkolivegreen', label='Messwerte', marker='.', zorder=1, linestyle='None')
+gausfit(gauss, xP1, yP1, 'blue', [0.6, 29.6, 0.05, 0, 0.3])
 plt.ylabel('Mittlere Zählrate [1/s]', fontsize=12, labelpad=2)
 plt.xlabel('Winkel [°]', fontsize=12, labelpad=1)
-
 secax = ax.secondary_xaxis('top', functions=(Deg2pm, pm2Deg))
 secax.set_xlabel('Wellenlänge [pm]', fontsize=12, labelpad=3)
 
-# thiax = ax.secondary_xaxis(-0.14)
-# thiax.set_xlabel('Energie [keV]', fontsize=12, labelpad=2)
+ax.axvline(pm2Deg(70.93+0.75), label = 'Literaturwerte')
+ax.axvline(pm2Deg(71.36+0.75))
+plt.legend(loc = 'upper left', framealpha=0.3)
 
-# ticks = np.array([6, 7, 8, 9, 10, 12, 15, 20, 30, 50])
-# tick_labels = [Ev2Deg(tick) for tick in ticks]
-# thiax.set_xticks(tick_labels )
-# thiax.set_xticklabels(ticks)
-
-plt.legend()
 plt.grid()
+ax.set_xlim(29.4, 29.8)
+plt.savefig(f'{path}\\FeinstrukturZoom.pdf')
+plt.cla()
+
+
+ax.axvline(pm2Deg(70.93), label = 'Literaturwerte')
+ax.axvline(pm2Deg(71.36))
+
+plt.errorbar(xData, yData, xerr=0.005, yerr=np.sqrt(120*yData)/120, color='darkolivegreen', label='Messwerte', marker='.', zorder=1, linestyle='None')
+gausfit(gauss, xP1, yP1, 'blue', [0.6, 29.6, 0.05, 0, 0.3])
+gausfit(gauss, xP2, yP2, 'crimson', [0.15, 31.6, 0.08, 0, 0.3])
+plt.ylabel('Mittlere Zählrate [1/s]', fontsize=12, labelpad=2)
+plt.xlabel('Winkel [°]', fontsize=12, labelpad=1)
+secax = ax.secondary_xaxis('top', functions=(Deg2pm, pm2Deg))
+secax.set_xlabel('Wellenlänge [pm]', fontsize=12, labelpad=3)
+
+thiax = ax.secondary_xaxis(-0.14)
+thiax.set_xlabel('Energie [keV]', fontsize=12, labelpad=2)
+
+ticks = np.array([15, 15.5, 16, 16.5, 17, 17.5, 18])
+tick_labels = [Ev2Deg(tick) for tick in ticks]
+thiax.set_xticks(tick_labels )
+thiax.set_xticklabels(ticks)
+plt.legend(loc = 'upper right', framealpha=0.8)
+plt.grid()
+ax.set_xlim(28.45, 32.05)
 plt.savefig(f'{path}\\Feinstruktur.pdf')
+
 
--- 428/Spektren.py
+++ 428/Spektren.py
@@ -3,30 +3,57 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import scipy.odr as odr
-import glob
-
 
 fig, ax = plt.subplots(layout='constrained')
 path = os.path.dirname(os.path.abspath(__file__))
 
+files = ['1unbek', '2unbek', '3unbek', '4unbek', 'AgK', 'AgL', 'Au', 'Cu', 'Fe', 'FeZn', 'In', 'Mo', 'Ni', 'Pb', 'Sn', 'Ti', 'W', 'Zn', 'Zr']
 
-# Verwende glob, um alle .txt Dateien im DatenD1 Ordner zu finden
-files = glob.glob(f'{path}\\DatenD2\\*.txt')
+
+def gausfit(func, x, y, beta):
+    Low = int(beta[1] - 3 * beta[2])
+    High = int(beta[1] + 3 * beta[2])
+    x = x[Low:High]
+    y = y[Low:High]
+    model = odr.Model(func)
+    mydata = odr.RealData(x, y, sx=0.005, sy=np.sqrt(y))
+    myodr = odr.ODR(mydata, model, beta0=beta)
+    out = myodr.run()
+    fy = func(out.beta, x)
+    chi2 = np.sum((y - fy)**2/np.sqrt(y))/(len(x) - len(out.beta))
+    # Printer(f'{out.beta[0]:.5g} $\pm$ {out.sd_beta[0]:.3g} & {out.beta[1]:.5g} $\pm$ {out.sd_beta[1]:.3g} & {out.beta[2]:.5g} $\pm$ {out.sd_beta[2]:.3g} & {out.beta[3]:.5g} $\pm$ {out.sd_beta[3]:.3g} & {out.beta[4]:.5g} $\pm$ {out.sd_beta[4]:.3g} & {chi2:.3g}')
+    plt.plot(x, fy,label = f'$\chi^2/ndf = {chi2:.3g} $', alpha = 0.7)
+    return out
+
+
+def Printer(Text):
+    Text = Text.replace('.', ',')
+    print(Text)
+
+
+def gauss(Para, x):
+    return Para[0] /(Para[2]*np.sqrt(2*np.pi))*np.exp(-0.5*((x - Para[1])/Para[2])**2)
+
 
 for file in files:
-    print(file[45:-4])
-    data = pd.read_csv(file, sep="\t", header=0, names=['b', 'R'], dtype=str)
-
-
+    data = pd.read_csv(f'{path}\\DatenD2\\{file}.txt', sep="\t", header=0, names=['b', 'R'], dtype=str)
+    Para = pd.read_csv(f'{path}\\Peaks\\{file}.peaks', sep="\t", header=0, names=['PeakType', 'Center', 'Height', 'Area', 'FWHM', 'Para'])
+    Para[['A', 'mu', 'sigma']] = Para['Para'].str.split(expand=True).astype(float)
     data['b'] = data['b'].str.replace(',', '.').astype(float)
     data['R'] = data['R'].str.replace(',', '.').astype(float)
-
     xData = data['b'].values
     yData = data['R'].values
 
-    plt.errorbar(xData, yData, xerr=0, yerr=np.sqrt(yData), label=f'{file[45:-4]}', marker='.', zorder=1, alpha = 0.5)
+    for i in range(len(Para)):
+        A = Para['A'][i]
+        mu = Para['mu'][i]
+        sigma = Para['sigma'][i]
+        print(A, mu, sigma)
+        out = gausfit(gauss, xData, yData, [A, mu, sigma])
+        yFit = gauss(out.beta, xData)
+        chi2 = np.sum((yData - yFit) ** 2 / np.sqrt(yData)) / (len(xData) - len(out.beta))
+    plt.errorbar(xData, yData, xerr=0.05, yerr=np.sqrt(yData), color='darkolivegreen', label='Messwerte', marker='.', zorder=1, linestyle='None')
     plt.grid()
     plt.legend()
-    plt.savefig(f'{path}\\Plots\\{file[45:-4]}.pdf')
+    plt.savefig(f'{path}\\Plots\\{file}.pdf')
     plt.cla()
-    plt.clf()
