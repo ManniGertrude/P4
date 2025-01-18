b42b493396b3d8cb83c5b048dc4ab79831b291f6
ManniGertrude <mannigertrude@gmail.com>
2025-01-18 14:15:38 +0100

    modified:   428/Peaks/1unbek.peaks 	modified:   428/Peaks/2unbek.peaks 	modified:   428/Peaks/4unbek.peaks 	modified:   428/Peaks/AgK.peaks 	modified:   428/Peaks/AgL.peaks 	modified:   428/Peaks/Au.peaks 	modified:   428/Peaks/Cu.peaks 	modified:   428/Peaks/In.peaks 	modified:   428/Peaks/Mo.peaks 	modified:   428/Peaks/Pb.peaks 	modified:   428/Peaks/Sn.peaks 	modified:   428/Peaks/W.peaks 	modified:   428/Plots/1unbek.pdf 	modified:   428/Plots/2unbek.pdf 	modified:   428/Plots/3unbek.pdf 	modified:   428/Plots/4unbek.pdf 	modified:   428/Plots/AgK.pdf 	modified:   428/Plots/AgL.pdf 	modified:   428/Plots/Au.pdf 	modified:   428/Plots/Cu.pdf 	modified:   428/Plots/Fe.pdf 	modified:   428/Plots/FeZn.pdf 	modified:   428/Plots/In.pdf 	modified:   428/Plots/Mo.pdf 	modified:   428/Plots/Ni.pdf 	modified:   428/Plots/PB.pdf 	modified:   428/Plots/Sn.pdf 	modified:   428/Plots/Ti.pdf 	modified:   428/Plots/W.pdf 	modified:   428/Plots/Zn.pdf 	modified:   428/Plots/Zr.pdf 	modified:   428/Spektren.py

--- 428/Spektren.py
+++ 428/Spektren.py
@@ -2,28 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import scipy.odr as odr
+from scipy.optimize import curve_fit
 
 fig, ax = plt.subplots(layout='constrained')
 path = os.path.dirname(os.path.abspath(__file__))
 
 files = ['1unbek', '2unbek', '3unbek', '4unbek', 'AgK', 'AgL', 'Au', 'Cu', 'Fe', 'FeZn', 'In', 'Mo', 'Ni', 'Pb', 'Sn', 'Ti', 'W', 'Zn', 'Zr']
 
-
-def gausfit(func, x, y, beta):
-    Low = int(beta[1] - 3 * beta[2])
-    High = int(beta[1] + 3 * beta[2])
-    x = x[Low:High]
-    y = y[Low:High]
-    model = odr.Model(func)
-    mydata = odr.RealData(x, y, sx=0.005, sy=np.sqrt(y))
-    myodr = odr.ODR(mydata, model, beta0=beta)
-    out = myodr.run()
-    fy = func(out.beta, x)
-    chi2 = np.sum((y - fy)**2/np.sqrt(y))/(len(x) - len(out.beta))
-    # Printer(f'{out.beta[0]:.5g} $\pm$ {out.sd_beta[0]:.3g} & {out.beta[1]:.5g} $\pm$ {out.sd_beta[1]:.3g} & {out.beta[2]:.5g} $\pm$ {out.sd_beta[2]:.3g} & {out.beta[3]:.5g} $\pm$ {out.sd_beta[3]:.3g} & {out.beta[4]:.5g} $\pm$ {out.sd_beta[4]:.3g} & {chi2:.3g}')
-    plt.plot(x, fy,label = f'$\chi^2/ndf = {chi2:.3g} $', alpha = 0.7)
-    return out
+def gauss(x, *p):
+    y = np.zeros_like(x)
+    for i in range(0, len(p), 3):
+        A = p[i]
+        mu = p[i+1]
+        sigma = p[i+2]
+        y = y + A * np.exp(-(x - mu)**2 / (2 * sigma**2))
+    return y
 
 
 def Printer(Text):
@@ -31,27 +24,24 @@ def Printer(Text):
     print(Text)
 
 
-def gauss(Para, x):
-    return Para[0] /(Para[2]*np.sqrt(2*np.pi))*np.exp(-0.5*((x - Para[1])/Para[2])**2)
-
-
 for file in files:
     data = pd.read_csv(f'{path}\\DatenD2\\{file}.txt', sep="\t", header=0, names=['b', 'R'], dtype=str)
     Para = pd.read_csv(f'{path}\\Peaks\\{file}.peaks', sep="\t", header=0, names=['PeakType', 'Center', 'Height', 'Area', 'FWHM', 'Para'])
-    Para[['A', 'mu', 'sigma']] = Para['Para'].str.split(expand=True).astype(float)
+    Params = Para['Para'].str.split(expand=True).astype(float).values.flatten()
     data['b'] = data['b'].str.replace(',', '.').astype(float)
     data['R'] = data['R'].str.replace(',', '.').astype(float)
     xData = data['b'].values
     yData = data['R'].values
 
-    for i in range(len(Para)):
-        A = Para['A'][i]
-        mu = Para['mu'][i]
-        sigma = Para['sigma'][i]
-        print(A, mu, sigma)
-        out = gausfit(gauss, xData, yData, [A, mu, sigma])
-        yFit = gauss(out.beta, xData)
-        chi2 = np.sum((yData - yFit) ** 2 / np.sqrt(yData)) / (len(xData) - len(out.beta))
+    popt, pcov = curve_fit(gauss, xdata=xData, ydata=yData, p0=Params)
+    fit = gauss(xData, *popt)
+    poptErr = np.sqrt(np.diag(pcov))
+    chi2 = np.sum((yData - fit)**2 / np.sqrt(yData+1e-5))/(len(yData) - len(Params))
+    print(f'\hline \hline \n {file} & {chi2} &  & \\\\ \n \hline')
+    for i in range(0, len(popt), 3):
+        Printer(f'{i//3+1} & {popt[i]:.0f} $\pm$ {poptErr[i]:.2g} & {popt[i+1]:.3f} $\pm$ {poptErr[i+1]:.2g} & {popt[i+2]:.4f} $\pm$ {poptErr[i+2]:.2g}\\\\')
+        plt.plot(xData, gauss(xData, *popt[i:i+3]), label=f'Gauss {i//3+1}', zorder=30, alpha=0.5, linestyle='--')
+    plt.plot(xData, fit, color='darkred', label=f'$\chi^2/ndf$ = {chi2:.2f}', zorder=2)
     plt.errorbar(xData, yData, xerr=0.05, yerr=np.sqrt(yData), color='darkolivegreen', label='Messwerte', marker='.', zorder=1, linestyle='None')
     plt.grid()
     plt.legend()
