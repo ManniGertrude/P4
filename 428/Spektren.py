a833b76fe274d24d2bcfb07a99ced1a6709a00ef
ManniGertrude <mannigertrude@gmail.com>
2025-01-19 14:54:49 +0100

    modified:   428/Plots/1unbek.pdf 	modified:   428/Plots/2unbek.pdf 	modified:   428/Plots/3unbek.pdf 	modified:   428/Plots/4unbek.pdf 	modified:   428/Plots/AgK.pdf 	modified:   428/Plots/AgL.pdf 	modified:   428/Plots/Au.pdf 	modified:   428/Plots/Cu.pdf 	modified:   428/Plots/Fe.pdf 	modified:   428/Plots/FeZn.pdf 	modified:   428/Plots/In.pdf 	modified:   428/Plots/Mo.pdf 	modified:   428/Plots/Ni.pdf 	modified:   428/Plots/PB.pdf 	modified:   428/Plots/Sn.pdf 	modified:   428/Plots/Ti.pdf 	modified:   428/Plots/W.pdf 	modified:   428/Plots/Zn.pdf 	modified:   428/Plots/Zr.pdf 	modified:   428/Spektren.py

--- 428/Spektren.py
+++ 428/Spektren.py
@@ -24,9 +24,9 @@ def Printer(Text):
     print(Text)
 
 
-for file in files:
-    data = pd.read_csv(f'{path}\\DatenD2\\{file}.txt', sep="\t", header=0, names=['b', 'R'], dtype=str)
-    Para = pd.read_csv(f'{path}\\Peaks\\{file}.peaks', sep="\t", header=0, names=['PeakType', 'Center', 'Height', 'Area', 'FWHM', 'Para'])
+for i in range(len(files)):
+    data = pd.read_csv(f'{path}\\DatenD2\\{files[i]}.txt', sep="\t", header=0, names=['b', 'R'], dtype=str)
+    Para = pd.read_csv(f'{path}\\Peaks\\{files[i]}.peaks', sep="\t", header=0, names=['PeakType', 'Center', 'Height', 'Area', 'FWHM', 'Para'])
     Params = Para['Para'].str.split(expand=True).astype(float).values.flatten()
     data['b'] = data['b'].str.replace(',', '.').astype(float)
     data['R'] = data['R'].str.replace(',', '.').astype(float)
@@ -37,13 +37,18 @@ for file in files:
     fit = gauss(xData, *popt)
     poptErr = np.sqrt(np.diag(pcov))
     chi2 = np.sum((yData - fit)**2 / np.sqrt(yData+1e-5))/(len(yData) - len(Params))
-    print(f'\hline \hline \n {file} & {chi2} &  & \\\\ \n \hline')
-    for i in range(0, len(popt), 3):
-        Printer(f'{i//3+1} & {popt[i]:.0f} $\pm$ {poptErr[i]:.2g} & {popt[i+1]:.3f} $\pm$ {poptErr[i+1]:.2g} & {popt[i+2]:.4f} $\pm$ {poptErr[i+2]:.2g}\\\\')
-        plt.plot(xData, gauss(xData, *popt[i:i+3]), label=f'Gauss {i//3+1}', zorder=30, alpha=0.5, linestyle='--')
+    print(f'\hline \hline \n {files[i]} & {chi2} &  & \\\\ \n \hline')
+    for j in range(0, len(popt), 3):
+        if i*100+j in [6, 106, 203, 306, 309, 415, 418, 506, 512, 612, 703, 803, 806, 809, 906, 903, 1118, 1124, 1203, 1312, 1403, 1606, 1703]:
+            Printer(f'({j//3+1}) & {popt[j]:.0f} $\pm$ {poptErr[j]:.2g} & {popt[j+1]:.3f} $\pm$ {poptErr[j+1]:.2g} & {popt[j+2]:.4f} $\pm$ {poptErr[j+2]:.2g}\\\\')
+            plt.plot(xData, gauss(xData, *popt[j:j+3]), label=f'Gauss ({j//3+1})', zorder=30, alpha=0.5, linestyle='--')
+
+        else:
+            Printer(f'{j//3+1} & {popt[j]:.0f} $\pm$ {poptErr[j]:.2g} & {popt[j+1]:.3f} $\pm$ {poptErr[j+1]:.2g} & {popt[j+2]:.4f} $\pm$ {poptErr[j+2]:.2g}\\\\')
+            plt.plot(xData, gauss(xData, *popt[j:j+3]), label=f'Gauss {j//3+1}', zorder=30, alpha=0.7, linestyle='--')
     plt.plot(xData, fit, color='darkred', label=f'$\chi^2/ndf$ = {chi2:.2f}', zorder=2)
     plt.errorbar(xData, yData, xerr=0.05, yerr=np.sqrt(yData), color='darkolivegreen', label='Messwerte', marker='.', zorder=1, linestyle='None')
     plt.grid()
     plt.legend()
-    plt.savefig(f'{path}\\Plots\\{file}.pdf')
+    plt.savefig(f'{path}\\Plots\\{files[i]}.pdf')
     plt.cla()
