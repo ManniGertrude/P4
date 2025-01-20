4dd34ceeb46dbb5c23e5fb1277ce83b2bdcb8468
ManniGertrude <mannigertrude@gmail.com>
2025-01-20 15:42:24 +0100

    modified:   428/Peaks/Mo.peaks 	modified:   428/Peaks/Ni.peaks 	modified:   428/Peaks/Ti.peaks 	modified:   428/Plots/1unbek.pdf 	modified:   428/Plots/2unbek.pdf 	modified:   428/Plots/3unbek.pdf 	modified:   428/Plots/4unbek.pdf 	modified:   428/Plots/AgK.pdf 	modified:   428/Plots/AgL.pdf 	modified:   428/Plots/Au.pdf 	modified:   428/Plots/Cu.pdf 	modified:   428/Plots/Fe.pdf 	modified:   428/Plots/FeZn.pdf 	modified:   428/Plots/In.pdf 	new file:   428/Plots/Kalibrierung.pdf 	modified:   428/Plots/Mo.pdf 	modified:   428/Plots/Ni.pdf 	modified:   428/Plots/PB.pdf 	modified:   428/Plots/Sn.pdf 	modified:   428/Plots/Ti.pdf 	modified:   428/Plots/W.pdf 	modified:   428/Plots/Zn.pdf 	modified:   428/Plots/Zr.pdf 	modified:   428/Spektren.py

--- 428/Spektren.py
+++ 428/Spektren.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from sklearn.metrics import r2_score
 from scipy.optimize import curve_fit
 
 fig, ax = plt.subplots(layout='constrained')
@@ -15,15 +16,19 @@ def gauss(x, *p):
         A = p[i]
         mu = p[i+1]
         sigma = p[i+2]
-        y = y + A * np.exp(-(x - mu)**2 / (2 * sigma**2))
+        y = y + A* np.exp(-(x - mu)**2 / (2 * sigma**2))
     return y
 
+
+def Linear(x, m, b):
+    return m*x + b
 
 def Printer(Text):
     Text = Text.replace('.', ',')
     print(Text)
 
-
+EKanal = []
+Sigma = []
 for i in range(len(files)):
     data = pd.read_csv(f'{path}\\DatenD2\\{files[i]}.txt', sep="\t", header=0, names=['b', 'R'], dtype=str)
     Para = pd.read_csv(f'{path}\\Peaks\\{files[i]}.peaks', sep="\t", header=0, names=['PeakType', 'Center', 'Height', 'Area', 'FWHM', 'Para'])
@@ -32,18 +37,21 @@ for i in range(len(files)):
     data['R'] = data['R'].str.replace(',', '.').astype(float)
     xData = data['b'].values
     yData = data['R'].values
-
     popt, pcov = curve_fit(gauss, xdata=xData, ydata=yData, p0=Params)
     fit = gauss(xData, *popt)
     poptErr = np.sqrt(np.diag(pcov))
     chi2 = np.sum((yData - fit)**2 / np.sqrt(yData+1e-5))/(len(yData) - len(Params))
+    #print(f'\hline \hline \n {files[i]} & \\\\ \n \hline')
     print(f'\hline \hline \n {files[i]} & {chi2} &  & \\\\ \n \hline')
     for j in range(0, len(popt), 3):
-        if i*100+j in [6, 106, 203, 306, 312, 415, 418, 506, 512, 612, 703, 800, 806, 809, 903, 1118, 1124, 1103, 1203, 1312, 1403, 1606, 1703]:
+        if i*100+j in [6, 106, 203, 306, 312, 415, 418, 506, 512, 612, 703, 903, 1118, 1124, 1103, 1203, 1312, 1403, 1503, 1606, 1703]:
             Printer(f'({j//3+1}) & {popt[j]:.0f} $\pm$ {poptErr[j]:.2g} & {popt[j+1]:.3f} $\pm$ {poptErr[j+1]:.2g} & {popt[j+2]:.4f} $\pm$ {poptErr[j+2]:.2g}\\\\')
             plt.plot(xData, gauss(xData, *popt[j:j+3]), label=f'Gauss ({j//3+1})', zorder=30, alpha=0.5, linestyle='--')
-
         else:
+            # print(f'{popt[j+1]/0.01106542 - 400.598:.0f} $\pm$ {np.sqrt((poptErr[j+1]/0.01106542)**2 + (2.44015004e-04*popt[j+1]/0.01106542**2)**2):.0f} &    \\\\')
+            if i in [7, 9, 12, 15]:
+                EKanal.append(popt[j+1])
+                Sigma.append(poptErr[j+1]*10)
             Printer(f'{j//3+1} & {popt[j]:.0f} $\pm$ {poptErr[j]:.2g} & {popt[j+1]:.3f} $\pm$ {poptErr[j+1]:.2g} & {popt[j+2]:.4f} $\pm$ {poptErr[j+2]:.2g}\\\\')
             plt.plot(xData, gauss(xData, *popt[j:j+3]), label=f'Gauss {j//3+1}', zorder=30, alpha=0.7, linestyle='--')
     plt.plot(xData, fit, color='darkred', label=f'$\chi^2/ndf$ = {chi2:.2f}', zorder=2)
@@ -55,3 +63,20 @@ for i in range(len(files)):
     plt.title(f'Spektrum {files[i]}', fontsize=14)
     plt.savefig(f'{path}\\Plots\\{files[i]}.pdf')
     plt.cla()
+
+
+
+x = [8979, 7112, 9659, 8333,  4966]
+popt, pcov = curve_fit(Linear, x, EKanal, sigma=Sigma)
+xVals = np.linspace(min(x), max(x), 10)
+r2 = r2_score(EKanal, Linear(np.array(x), *popt))
+plt.errorbar(x=x, y=EKanal, yerr=Sigma, color='darkred', label='Mittelwerte', marker='.', linestyle='None', capsize=3)
+plt.plot(xVals, Linear(np.array(xVals), *popt), color='darkblue', label=f'Anpassung mit $R^2$ = {r2:.4f}', zorder=2)
+#print(popt, np.sqrt(np.diag(pcov)))
+plt.xlabel('Energie der Linie [eV]', fontsize=12)
+plt.ylabel('Kanalnummer des MCA', fontsize=12)
+plt.title('Kalibrierung', fontsize=14)
+plt.grid()
+plt.legend()
+plt.savefig(f'{path}\\Plots\\Kalibrierung.pdf')
+
