eb23894a66becbfe047347e1fe64408cfb36df37
ManniGertrude <mannigertrude@gmail.com>
2025-02-21 11:41:52 +0100

    new file:   464/BAurOut/2VerschiebungFe.pdf 	new file:   464/BAurOut/2VerschiebungHa.pdf 	new file:   464/BAurOut/2VerschiebungS1.pdf 	new file:   464/BAurOut/2VerschiebungS2.pdf 	modified:   464/BAurOut/Spektrum2021-02-09.pdf 	modified:   464/BAurOut/Spektrum2021-02-11A.pdf 	modified:   464/BAurOut/Spektrum2021-02-11B.pdf 	modified:   464/BAurOut/Spektrum2021-02-12A.pdf 	modified:   464/BAurOut/Spektrum2021-02-12C.pdf 	modified:   464/BAurOut/Spektrum2021-02-13A.pdf 	modified:   464/BAurOut/Spektrum2021-02-13B.pdf 	modified:   464/BAurOut/Spektrum2021-02-13C.pdf 	modified:   464/BAurOut/Spektrum2021-04-23.pdf 	modified:   464/BAurOut/Spektrum2021-09-01.pdf 	modified:   464/BAurOut/Spektrum2021-09-07.pdf 	modified:   464/BAurOut/Spektrum2021-09-08.pdf 	modified:   464/BAurOut/Spektrum2021-09-22.pdf 	modified:   464/BAurOut/Spektrum2021-10-07.pdf 	modified:   464/BAurOut/Spektrum2021-10-27.pdf 	modified:   464/BAurOut/Spektrum2021-10-28.pdf 	modified:   464/BAurOut/Spektrum2021-11-09.pdf 	modified:   464/BAurOut/Spektrum2021-12-20A.pdf 	modified:   464/BAurOut/Spektrum2021-12-20B.pdf 	modified:   464/BAurOut/Spektrum2021-12-21B.pdf 	modified:   464/BAurOut/VerschiebungFe.pdf 	modified:   464/BAurOut/VerschiebungHa.pdf 	modified:   464/BAurOut/VerschiebungS1.pdf 	modified:   464/BAurOut/VerschiebungS2.pdf 	modified:   464/BAurOut/fit2021-02-09.pdf 	modified:   464/BAurOut/fit2021-02-11A.pdf 	modified:   464/BAurOut/fit2021-02-11B.pdf 	modified:   464/BAurOut/fit2021-02-12A.pdf 	modified:   464/BAurOut/fit2021-02-12C.pdf 	modified:   464/BAurOut/fit2021-02-13A.pdf 	modified:   464/BAurOut/fit2021-02-13B.pdf 	modified:   464/BAurOut/fit2021-02-13C.pdf 	modified:   464/BAurOut/fit2021-04-23.pdf 	modified:   464/BAurOut/fit2021-09-01.pdf 	modified:   464/BAurOut/fit2021-09-07.pdf 	modified:   464/BAurOut/fit2021-09-08.pdf 	modified:   464/BAurOut/fit2021-09-22.pdf 	modified:   464/BAurOut/fit2021-10-07.pdf 	modified:   464/BAurOut/fit2021-10-27.pdf 	modified:   464/BAurOut/fit2021-10-28.pdf 	modified:   464/BAurOut/fit2021-11-09.pdf 	modified:   464/BAurOut/fit2021-12-20A.pdf 	modified:   464/BAurOut/fit2021-12-20B.pdf 	modified:   464/BAurOut/fit2021-12-21B.pdf 	modified:   464/BAurOut/output2021-02-09.pdf 	modified:   464/BAurOut/output2021-02-11A.pdf 	modified:   464/BAurOut/output2021-02-11B.pdf 	modified:   464/BAurOut/output2021-02-12A.pdf 	modified:   464/BAurOut/output2021-02-12C.pdf 	modified:   464/BAurOut/output2021-02-13A.pdf 	modified:   464/BAurOut/output2021-02-13B.pdf 	modified:   464/BAurOut/output2021-02-13C.pdf 	modified:   464/BAurOut/output2021-04-23.pdf 	modified:   464/BAurOut/output2021-09-01.pdf 	modified:   464/BAurOut/output2021-09-07.pdf 	modified:   464/BAurOut/output2021-09-08.pdf 	modified:   464/BAurOut/output2021-09-22.pdf 	modified:   464/BAurOut/output2021-10-07.pdf 	modified:   464/BAurOut/output2021-10-27.pdf 	modified:   464/BAurOut/output2021-10-28.pdf 	modified:   464/BAurOut/output2021-11-09.pdf 	modified:   464/BAurOut/output2021-12-20A.pdf 	modified:   464/BAurOut/output2021-12-20B.pdf 	modified:   464/BAurOut/output2021-12-21B.pdf 	modified:   464/doppelstern.py

--- 464/doppelstern.py
+++ 464/doppelstern.py
@@ -17,7 +17,7 @@ def linear(x, a, b):
     return a * x + b 
 
 def sin(x, a, b):
-    return a *abs(np.sin(2*np.pi*(x+b)/3.9600400))
+    return a *abs(np.sin(np.deg2rad(76.8))*np.sin(2*np.pi*(x+b)/3.9600400))
 
 DateTable = np.array(['2021-02-09T23:17:09', 
              '2021-02-11T19:28:29',
@@ -67,7 +67,7 @@ for folder in os.listdir(f'{path}\\BAur'):
     fig, ax = plt.subplots()
         
     Data = pd.read_csv(f'{path}\\BAur\\{folder}\\output.dat', sep='\t', names=['x', 'b', 'y'])[:742]
-    ax.plot(Data['x'], Data['y'], label=f'{folder} (Original)')
+    # ax.plot(Data['x'], Data['y'], label=f'{folder} (Original)')
     Dataf = Data
     Maxima = []
     for i in range(6):
@@ -75,60 +75,95 @@ for folder in os.listdir(f'{path}\\BAur'):
         mask = ~((Dataf['x'] > Maxima[i] - 10) & (Dataf['x'] < Maxima[i] + 10))
         Dataf = Dataf[mask]
         if i == 3:
-            ax.plot(Dataf['x'][150:], Dataf['y'][150:], color='green')
+            # ax.plot(Dataf['x'][150:], Dataf['y'][150:], color='green')
             mask = ~((Dataf['x'] > 200))
             Dataf = Dataf[mask]
 
     Maxima = np.sort(Maxima)
     
-    ax.plot(Dataf['x'], Dataf['y'], label=f'{folder} (Gefiltert)', color='green')
-    plt.legend()
-    plt.grid()
-    plt.xlabel('Kanalnummer')
-    plt.ylabel('Counts')
-    plt.savefig(f'{path}\\BAurOut\\output{folder}.pdf')
-    ax.cla()
+    # ax.plot(Dataf['x'], Dataf['y'], label=f'{folder} (Gefiltert)', color='green')
+    # plt.legend()
+    # plt.grid()
+    # plt.xlabel('Kanalnummer')
+    # plt.ylabel('Counts')
+    # plt.savefig(f'{path}\\BAurOut\\output{folder}.pdf')
+    # ax.cla()
     
     
     popt, pcov = curve_fit(linear, Maxima, Lambda, p0=[1, 1])
     R2score = 1 - r2_score(Lambda, linear(np.array(Maxima), *popt))
-    ax.plot(Maxima, linear(np.array(Maxima), *popt), label=f'{popt[0]:.4f} * x + {popt[1]:.0f} mit $R^2$ = 1 - {R2score:.2g}')
-    ax.plot(Maxima, Lambda, 'x', label=f'{folder} (Maxima)', color='crimson')
-    plt.legend()
-    plt.grid()
-    plt.ylabel('Wellenlänge (Angström)')
-    plt.xlabel('Kanalnummer')
-    plt.savefig(f'{path}\\BAurOut\\fit{folder}.pdf')
-    ax.cla() 
+    # ax.plot(Maxima, linear(np.array(Maxima), *popt), label=f'{popt[0]:.4f} * x + {popt[1]:.0f} mit $R^2$ = 1 - {R2score:.2g}')
+    # ax.plot(Maxima, Lambda, 'x', label=f'{folder} (Maxima)', color='crimson')
+    # plt.legend()
+    # plt.grid()
+    # plt.ylabel('Wellenlänge (Angström)')
+    # plt.xlabel('Kanalnummer')
+    # plt.savefig(f'{path}\\BAurOut\\fit{folder}.pdf')
+    # ax.cla() 
     Error = np.sqrt(np.diag(pcov))
     # Printer(f'{Index+1} & {popt[0]:.5f} $\pm$ {Error[0]:.5f} & {popt[1]:.2f} $\pm$ {Error[1]:.2g} & {1-R2score:.6f} \\\\')
     
-    ax.errorbar(linear(Data['x'], *popt), Data['b'], yerr=np.sqrt(Data['b']), label=f'{folder}')
-    for k in range(4):
-        ax.axvline(x=Linien[names1[k]][Index], color = 'red', linestyle='--', alpha=0.5)
-        ax.axvline(x=Linien[names2[k]][Index], color = 'green', linestyle='--', alpha=0.5)
-    plt.legend(loc='upper right')
-    plt.grid()
-    plt.xlabel('Wellenlänge in Angström', fontsize=12)
-    plt.ylabel('Intensität', fontsize=12)
-    plt.title(f'Intenistätsspektrum von {folder.replace("-"," ")}', fontsize=14)
-    plt.savefig(f'{path}\\BAurOut\\Spektrum{folder}.pdf')
-    # plt.show()
-    ax.cla()
+    # ax.errorbar(linear(Data['x'], *popt), Data['b'], yerr=np.sqrt(Data['b']), label=f'{folder}')
+    # for k in range(4):
+    #     ax.axvline(x=Linien[names1[k]][Index], color = 'red', linestyle='--', alpha=0.5)
+    #     ax.axvline(x=Linien[names2[k]][Index], color = 'green', linestyle='--', alpha=0.5)
+    # plt.legend(loc='upper right')
+    # plt.grid()
+    # plt.xlabel('Wellenlänge in Angström', fontsize=12)
+    # plt.ylabel('Intensität', fontsize=12)
+    # plt.title(f'Intenistätsspektrum von {folder.replace("-"," ")}', fontsize=14)
+    # plt.savefig(f'{path}\\BAurOut\\Spektrum{folder}.pdf')
+    # # plt.show()
+    # ax.cla()
 
-
-
+def Linienverschiebung(Zeitpunkt, b=0):
+    LE = 2*np.pi*Zeitpunkt/365.25
+    vErde = -29800*np.cos(B)*np.sin(LE-L) + b
+    return vErde
 Linie = [6347.1, 6371.4, 6456.4, 6562.8]
-LE = 2*np.pi*ZeitVerschiebung/365.25
-vErde = -29800*np.cos(B)*np.sin(LE-L)
-XValues = np.linspace(min(ModZeitVerschiebung), max(ModZeitVerschiebung), 1000)
+vErde = Linienverschiebung(ZeitVerschiebung)
+XValues = np.linspace(min(ZeitVerschiebung), max(ZeitVerschiebung), 1000)
+ModXValues = np.linspace(min(ModZeitVerschiebung), max(ModZeitVerschiebung), 1000)
+
 Colortable = ['crimson', 'blue', 'green', 'purple']
+
+
+
+
+
+
 for i in range(4):
-    Verschiebung = abs((Linien[names1[i]]-Linie[i])*3e8/Linie[i]  - ((Linien[names2[i]]-Linie[i])*3e8/Linie[i]))
-    VerschiebungErr = np.sqrt((0.5*3e8/Linie[i])**2 + (0.05*(Linien[names1[i]]-Linie[i])*3e8/Linie[i]**2)**2)
+    Abw = abs(Linien[names1[i]]-Linien[names2[i]])
+    Verschiebung = []
+    TempZeitverschiebung = []
+    for j in range(len(Abw)):
+        if Abw[j] < 0.2:
+            TempZeitverschiebung.append(ZeitVerschiebung[j])
+            Verschiebung.append(0.5*(Linien[names1[i]][j] + Linien[names2[i]][j])  - Linie[i])
+            ax.errorbar(ZeitVerschiebung[j], Verschiebung[-1], color=Colortable[i], yerr = 0.5, linestyle='none', marker='x', capsize=2)
+    popt, pcov = curve_fit(Linienverschiebung, TempZeitverschiebung, Verschiebung, sigma=[0.05]*len(Verschiebung), absolute_sigma=True, p0=[0], maxfev=10000)
+    plt.plot(XValues, Linienverschiebung(XValues, popt)/(3*Linie[i]), label=names1[i][:-1], color=Colortable[i])
+    # print(names1[i][:-1], popt, np.sqrt(np.diag(pcov)))
+plt.grid()
+plt.legend()
+plt.xlabel('Zeit in Tagen')
+plt.ylabel('Abweichung in Angström')
+plt.title('Dopplerverschiebung zum Doppelsternsystem')
+plt.savefig(f'{path}\\BAurOut\\Verschiebung.pdf')
+plt.cla()
+
+
+
+
+
+
+for i in range(4):
+    LinieNeu = Linie[i] + Linienverschiebung(ZeitVerschiebung)/(3*Linie[i])
+    Verschiebung = abs((Linien[names1[i]]-LinieNeu)*3e5/LinieNeu - (Linien[names2[i]]-LinieNeu)*3e5/LinieNeu)
+    VerschiebungErr = np.sqrt((0.5*3e5/LinieNeu)**2 + (0.05*(Linien[names1[i]]-LinieNeu)*3e5/LinieNeu**2)**2)
     plt.errorbar(ModZeitVerschiebung, Verschiebung, yerr=VerschiebungErr, elinewidth=0.1, linestyle='none', label = names1[i][:-1], color=Colortable[i], marker='x')
-    popt, pcov = curve_fit(sin, ModZeitVerschiebung, Verschiebung, sigma=VerschiebungErr, absolute_sigma=True, p0=[1, 1], maxfev=10000)
-    plt.plot(XValues, sin(XValues, *popt), color = Colortable[i],)
+    popt, pcov = curve_fit(sin, ModZeitVerschiebung, Verschiebung, sigma=VerschiebungErr, absolute_sigma=True, p0=[200, 1.3], maxfev=10000)
+    plt.plot(ModXValues, sin(ModXValues, *popt), color = Colortable[i],)
     plt.grid()
     plt.legend()
     plt.xlabel('Zeit in Tagen')
@@ -136,9 +171,24 @@ for i in range(4):
     plt.savefig(f'{path}\\BAurOut\\Verschiebung{names1[i][:-1]}.pdf')
     ax.cla()
     chi2red = np.sum(((Verschiebung - sin(ModZeitVerschiebung, *popt))**2)/VerschiebungErr**2)/(len(ModZeitVerschiebung)-2)
-    # print(popt, np.sqrt(np.diag(pcov)))
-    # print(chi2red)
+    Error = np.sqrt(np.diag(pcov))
+    # Printer(f'{names1[i][:-1]} & {popt[0]:.1f} $\pm$ {Error[0]:.1f} & {popt[1]%3.96004:.3f} $\pm$ {Error[1]:.3f} & {chi2red:.3f} \\\\')
 
-# for i in range(len(Linien)):
-#     Printer(f'{i+1} & {Linien["Ha1"][i]:.1f} & {Linien["Ha2"][i]:.1f} & {Linien["S11"][i]:.1f} & {Linien["S12"][i]:.1f} & {Linien["S21"][i]:.1f} &  {Linien["S22"][i]:.1f} & {Linien["Fe1"][i]:.1f} & {Linien["Fe2"][i]:.1f} \\\\')
+def Sinus(x, a, b):
+    return a *np.sin(np.deg2rad(76.8))*np.sin(2*np.pi*(x+b)/3.9600400)
+
+for i in range(4):
+    LinieNeu = Linie[i] + Linienverschiebung(ZeitVerschiebung)/(3*Linie[i])
+    Verschiebung = abs((Linien[names1[i]]-LinieNeu)*3e5/LinieNeu - (Linien[names2[i]]-LinieNeu)*3e5/LinieNeu)
+    VerschiebungErr = np.sqrt((0.5*3e5/LinieNeu)**2 + (0.05*(Linien[names1[i]]-LinieNeu)*3e5/LinieNeu**2)**2)
+    plt.errorbar(ModZeitVerschiebung, Verschiebung, yerr=VerschiebungErr, elinewidth=0.1, linestyle='none', label = names1[i][:-1], color=Colortable[i], marker='x')
+    plt.plot(ModXValues, Sinus(ModXValues, 211.2, 1.267), color = Colortable[i])
+    plt.plot(ModXValues, Sinus(ModXValues, 229.6, 1.267+3.96004/2), color = Colortable[i])
+    plt.grid()
+    plt.legend()
+    plt.xlabel('Zeit in Tagen')
+    plt.ylabel('Geschwindigkeit zueinander km/s')
+    plt.savefig(f'{path}\\BAurOut\\Verschiebung2{names1[i][:-1]}.pdf')
+    ax.cla()
+    chi2red = np.sum(((Verschiebung - Sinus(ModZeitVerschiebung, *popt))**2)/VerschiebungErr**2)/(len(ModZeitVerschiebung)-2)
 
